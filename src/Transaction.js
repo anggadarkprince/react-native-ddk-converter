0a6e8bb487e8d5d6d07ea2205dc8f253ddd5493a
Angga Ari Wijaya <anggadarkprince@gmail.com>
2019-12-03 19:16:15 +0700

    add reload button

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { WebView, View } from 'react-native';
+import { Button } from './components/interaction';
 import Header from './Header';
 
-const Transaction = (props) => {
-    return (
-        <View style={styles.container}>
-            <Header title="Transactions" />
-            <WebView
-                source={{
-                    uri: 'https://ddkexplorer.ddkoin.com/account/9800187006007272034'
-                }}
-            />
-        </View>
-    );
+class Transaction extends Component {
+    reload() {
+        this.myWebView.reload();
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header title="Transactions" />
+                <WebView
+                    source={{
+                        uri: 'https://ddkexplorer.ddkoin.com/account/9800187006007272034'
+                    }}
+                    ref={(ref) => this.myWebView = ref}
+                />
+                <View style={styles.buttonWrapper}>
+                    <Button onPress={this.reload.bind(this)}>
+                        Refresh
+                    </Button>
+                </View>
+            </View>
+        );
+    }
 };
 
 const styles = {
@@ -20,6 +32,19 @@ const styles = {
         flex: 1,
         backgroundColor: '#fff',
     },
+
+    buttonWrapper: {
+        borderBottomWidth: 1,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
+    }
 };
 
 export default Transaction;
